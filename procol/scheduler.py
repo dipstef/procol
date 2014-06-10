35b3b537044c240bc41e577a78f23f25c8ab9a87
dipstef <dipstef@gmail.com>
2014-06-10 12:23:59 +0100

    Updated scheduler

--- procol/scheduler.py
+++ procol/scheduler.py
@@ -1,16 +1,60 @@
-from threading import Timer
+import atexit
+from threading import Timer, Event
+
+from dated.date_time import to_seconds
 
 
-def schedule_function(seconds, fun, daemon=False):
-    timer = Timer(seconds, fun)
-    timer.daemon = daemon
-    timer.start()
+class Repeater(object):
+
+    def __init__(self, fun, after, every=None):
+        self._seconds_repeat = to_seconds(every or after)
+        self._fun = fun
+        self._stopped = Event()
+
+        self._timer = self._schedule(to_seconds(after))
+        atexit.register(self._exit)
+
+    def start(self):
+        self._timer.start()
+
+    def _repeat(self):
+        self._fun()
+        if not self._stopped.is_set():
+            self._timer = self._schedule(self._seconds_repeat)
+            self._timer.start()
+
+    def _schedule(self, secs):
+        timer = Timer(secs, self._repeat)
+        timer.daemon = True
+        return timer
+
+    def _exit(self):
+        self._stopped.set()
+        self._timer.join()
+
+    def cancel(self):
+        self._stopped.set()
+        self._timer.cancel()
 
 
-def schedule_every(seconds, fun, daemon=True):
+class Scheduler(object):
 
-    def schedule_after():
-        fun()
-        schedule_function(seconds, schedule_after, daemon=daemon)
+    def __init__(self, fun):
+        self._fun = fun
 
-    schedule_function(seconds, schedule_after, daemon=daemon)
+    def __call__(self, after, repeat=False):
+        timer = Repeater(self._fun, after) if repeat else Timer(to_seconds(after), self._fun)
+        timer.start()
+        return timer
+
+
+def schedule(fun, after, repeat=False):
+    scheduler = Scheduler(fun)
+
+    return scheduler(after, repeat=repeat)
+
+
+def repeat(fun, every, after=None):
+    repeater = Repeater(fun, after=after or every, every=every)
+    repeater.start()
+    return repeater
--- tests/scheduler_test.py
+++ tests/scheduler_test.py
@@ -1,20 +1,16 @@
-from procol.scheduler import schedule_every
+from dated.normalized import utc
+from dated.date_time import seconds
+import time
+from procol.scheduler import schedule
 
 
 def main():
-    from datelib.normalized import utc
+    def print_hello(secs):
+        print 'Hello every %d seconds : %s ' % (secs, utc.now())
 
-    def print_hello(seconds):
-        print 'Hello every %d seconds : %s: ' % (seconds, utc.now())
+    schedule(lambda: print_hello(2), after=seconds(2), repeat=True)
 
-    #print utc.now()
-
-    schedule_every(2, lambda: print_hello(2), daemon=False)
-
-    #schedule_function(10, lambda: print_hello(10))
-    #schedule_function(5, lambda: print_hello(5))
-    print 'OK'
-    print 'END'
+    time.sleep(10)
 
 if __name__ == '__main__':
     main()
