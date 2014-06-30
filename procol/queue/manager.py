d158dae315e84a44f3f3dbcf0cb2798451357854
dipstef <dipstef@gmail.com>
2014-07-01 00:48:19 +0100

    updated remote queue

--- procol/queue/manager.py
+++ procol/queue/manager.py
@@ -7,14 +7,13 @@ class QueueManager(SyncManager):
 
 def queue_manager(queue, address=None):
     QueueManager.register('get_queue', callable=lambda: queue)
+    queue.start()
 
     return QueueManager(address=address, authkey='123456')
 
 
 def queue_manager_process(queue):
     manager = queue_manager(queue)
-
-    queue.start()
     manager.start()
 
     manager_queue = manager.get_queue()
@@ -25,7 +24,6 @@ def queue_server(queue, port=50000, serve_locally=True):
     host = '127.0.0.1' if serve_locally else '0.0.0.0'
 
     manager = queue_manager(queue, address=(host, port))
-    queue.start()
     server = manager.get_server()
 
     print 'Serving on port: ', port
@@ -38,6 +36,6 @@ def queue_client(server_address=None):
     queue = QueueManager(address=server_address, authkey='123456')
     queue.connect()
 
-    pool = queue.get_queue()
+    queue = queue.get_queue()
 
-    return pool
+    return queue
